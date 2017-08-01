49f80835de9d52a5df8a7ea25ef9767b6cb2b0d8
southnan0 <1915250964@qq.com>
2017-08-01 22:56:57 +0800

    增加方向和时间限制

--- turn-around/src/StartScene.js
+++ turn-around/src/StartScene.js
@@ -1,6 +1,14 @@
 //todo 随机出个小人
 
 var StartLayer = cc.Layer.extend({
+    speed: 5000,
+    times: 0,
+    failTimes: 0,
+    largeFailTime: 3,
+    curPosition: Math.round(Math.random() * 3),
+    timer: null,
+    arrPosition: ['top', 'right', 'bottom', 'left'],
+    label: null,
     ctor: function () {
         this._super();
 
@@ -17,6 +25,7 @@ var StartLayer = cc.Layer.extend({
         this.addButton('right', size.width - 40, size.height / 2 - 15);
         this.addButton('bottom', size.width / 2, 40);
         this.addButton('left', 30, size.height / 2 - 15);
+        this.showPosition();
     },
     addSoldier: function () {
         this.solider = new SoldierSprite(res.solider);
@@ -32,6 +41,20 @@ var StartLayer = cc.Layer.extend({
 
         this.addChild(this.solider, 0);
     },
+    showPosition: function () {
+        const _self = this;
+        clearTimeout(this.timer);
+        if (_self.failTimes > _self.largeFailTime) return console.info('game over');
+        _self.curPosition = Math.round(Math.random() * 3);
+
+        _self.addLabel.call(_self);
+        this.speed = this.speed - 50;
+        console.info(this.speed)
+        this.timer = setTimeout(function () {
+            _self.failTimes++;
+            _self.showPosition();
+        }, this.speed);
+    },
     addButton: function (id, x, y) {
         var size = cc.winSize;
         var _self = this;
@@ -39,15 +62,25 @@ var StartLayer = cc.Layer.extend({
             res.solider,
             res.soliderActive,
             function (button) {
-                switch(button.getName()){
-                    case 'top':
-                        return _self.solider.rotation = 0;
-                    case 'right':
-                        return _self.solider.rotation = 90;
-                    case 'bottom':
-                        return _self.solider.rotation = 180;
-                    case 'left':
-                        return _self.solider.rotation = 270;
+                if (button.getName() === _self.arrPosition[_self.curPosition]) {
+                    _self.times++;
+                    _self.showPosition();
+                    switch (button.getName()) {
+                        case 'top':
+                            return _self.solider.rotation = 0;
+                        case 'right':
+                            return _self.solider.rotation = 90;
+                        case 'bottom':
+                            return _self.solider.rotation = 180;
+                        case 'left':
+                            return _self.solider.rotation = 270;
+                    }
+                } else {
+                    _self.failTimes++;
+                    if (_self.failTimes === _self.largeFailTime) {
+                        return console.info('game over')
+                    }
+                    _self.showPosition();
                 }
             }
         );
@@ -66,6 +99,17 @@ var StartLayer = cc.Layer.extend({
         this.addChild(button, 1);
 
         return true;
+    },
+    addLabel: function () {
+        this.label && this.removeChild(this.label);
+        var size = cc.winSize;
+        var helloLabel = new cc.LabelTTF.create(this.arrPosition[this.curPosition], "Times New Roman", 32);
+        helloLabel.x = size.width / 2;
+        helloLabel.y = size.height / 2;
+        helloLabel.color = '#000';
+        helloLabel.background = '#fff';
+        this.label = helloLabel;
+        this.addChild(helloLabel);
     }
 });
 
